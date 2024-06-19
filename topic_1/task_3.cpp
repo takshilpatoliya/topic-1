2870ade1d648d0ca74937a5ff10edd87f8955abd
takshilpatoliya <takshilpatoliya74@gmail.com>
2024-06-19 11:34:44 +0530

    Update task_3.cpp

--- topic_1/task_3.cpp
+++ topic_1/task_3.cpp
@@ -4,12 +4,12 @@ main()
 {
 	printf("---------\n");
 	printf("|\t|\n");
-	printf("H\t|\n");
-	printf("A\t|\n");
-	printf("R\t|\n");
-	printf("D\t|\n");
-	printf("I\t|\n");
-	printf("K\t|\n");
-	printf("|\t|\n");
+	printf("t\t|\n");
+	printf("a\t|\n");
+	printf("k\t|\n");
+	printf("s\t|\n");
+	printf("h\t|\n");
+	printf("i\t|\n");
+	printf("l\t|\n");
 	printf("---------\n");
 }
